c9f8b224099bbd870bd9dd2760c1fe87d3b045a8
Ajay Gnanasambanthan <agnanasa@gmail.com>
2024-09-18 13:05:00 -0700

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,14 @@
+import math
+def lcm(a, b):
+    return(math.lcm(a,b))
+    #for l in range(1, a * b + 1):
+     #   if l % a == 0 and l % b == 0:
+      #      return l
+
+    #assert False
+
+
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(lcm(a, b))
+
